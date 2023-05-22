8a5b5da7ba6900f73b93053ba1ce609cd0dddd6c
Vinicius Barros <vinnicius9511@gmail.com>
2023-05-22 20:15:18 -0300

    implementing new event <abstraction> structure

--- events.h
+++ events.h
@@ -22,8 +22,8 @@ enum EVENT_STATUS{
 #define EVENT_OFF EVENT_OFF
 };
 
-static void read_data(int fd,void*arg,long time_now);
-static void accept_con(int fd,void*arg,long time_now);
+static void read_data(int fd,void*arg,long time_now,eventloop* ev);
+static void accept_con(int fd,void*arg,long time_now,eventloop *ev);
 typedef void event_handler (int,void*,long);
 
 typedef struct s_event {
@@ -35,22 +35,24 @@ typedef struct s_event {
     void *arg;
 }s_event;
 
-extern int epollfd;                                        //Global epoll file descriptor (returned by epoll create)
-extern s_event events_t[EVENTS_MAX + 1];                    //Global events table
+typedef struct ev {
+    int epollfd;                                        //Global epoll file descriptor (returned by epoll create)
+    s_event events_t[EVENTS_MAX + 1];                    //Global events table
+}eventloop;
 
-static void read_data(int fd,void*arg,long time_now){
-    s_event *ev = (s_event*) arg;
+static void read_data(int fd,void*arg,long time_now,eventloop *ev){
+    s_event *aux_ev = (s_event*) arg;
     char buf[BUFFLEN];
     int len;
     len = read(fd,buf,sizeof(buf));
 
     if(len==0){
-        event_rm(ev,epollfd);
+        event_rm(aux_ev,epollfd);
         close(fd);
         printf("connection closed by client\n");
         return;
     }else if(len<0){
-        event_rm(ev,epollfd);
+        event_rm(aux_ev,epollfd);
         close(fd);
         printf("%s read(), error: %s\n",__func__,strerror(errno));
         return;
@@ -59,7 +61,7 @@ static void read_data(int fd,void*arg,long time_now){
     //At this point, the read data would be processed
 
     if(write(fd,"OK",3)!=3){
-        event_rm(ev,epollfd);
+        event_rm(aux_ev,epollfd);
         printf("failed to send all data\n");
 	    close(fd); /* failed to send all data at once, close */
         return;
@@ -67,7 +69,7 @@ static void read_data(int fd,void*arg,long time_now){
     ev->last_active=time_now;
 }
 
-static void accept_con(int fd,void*arg,long time_now)
+static void accept_con(int fd,void*arg,long time_now,eventloop *ev)
 {
     struct sockaddr_in cin;
     socklen_t addr_len = sizeof(cin);
@@ -79,7 +81,7 @@ static void accept_con(int fd,void*arg,long time_now)
     }
     //finds first available event on events table
     for (i = 0; i < EVENTS_MAX; i++) 
-        if (events_t[i].status == EVENT_OFF)                              
+        if (ev->events_t[i].status == EVENT_OFF)                              
             break;       
 
     if (i == EVENTS_MAX) {
@@ -95,10 +97,10 @@ static void accept_con(int fd,void*arg,long time_now)
     }
 
     // registers read_data() as the event_handler callback function 
-    event_set(&events_t[i],cfd,read_data,&events_t[i],time_now);  
+    event_set(&ev->events_t[i],cfd,read_data,&ev->events_t[i],time_now);  
 
     // adds the EPOLLIN event as the interested event for the newly createad connection (events_t[i]->fd)
-    event_add(&events_t[i],epollfd, EPOLLIN);    
+    event_add(&ev->events_t[i],ev->epollfd, EPOLLIN);    
 }
 
 #endif
