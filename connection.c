991a9b969fbbfb1981bb39e1b7952cd376ff431c
Vinicius Barros <vinnicius9511@gmail.com>
2023-05-23 22:03:21 -0300

    creating connection specific headers and files

---
+++ connection.c
@@ -0,0 +1,37 @@
+/*
+ * connection.c
+ *
+ *  Created on: 2023-05-23
+ *      Author: @v-barros
+ */
+#include "connection.h"
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <assert.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+int init_conn(int port){
+
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port);
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    assert(sockfd !=-1);
+    // set socket to NON BLOCKING
+    fcntl(sockfd, F_SETFL, O_NONBLOCK);
+    
+    assert((bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))) == 0);
+   
+    assert((listen(sockfd, 20)) == 0);
+
+    return sockfd;
+        
+}
---
+++ connection.h
@@ -0,0 +1,12 @@
+/*
+ * connection.h
+ *
+ *  Created on: 2023-05-23
+ *      Author: @v-barros
+ */
+#ifndef CONNECTION_H_
+#define CONNECTION_H_
+// set up new listening socket on fd using port
+int init_conn(int port);
+
+#endif // CONNECTION_H_
