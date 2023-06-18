29b34b54ae05b009635a364d7742368bbdb98b93
Vinicius Barros <vinnicius9511@gmail.com>
2023-06-18 12:37:02 -0300

    create new connection-related structures

--- connection.h
+++ connection.h
@@ -6,7 +6,50 @@
  */
 #ifndef CONNECTION_H_
 #define CONNECTION_H_
+struct aeEventLoop;
 
+
+typedef struct connection connection;
+typedef void (*ConnectionCallbackFunc)(struct connection *conn);
+typedef enum {
+    CONN_STATE_NONE = 0,
+    CONN_STATE_CONNECTING,
+    CONN_STATE_ACCEPTING,
+    CONN_STATE_CONNECTED,
+    CONN_STATE_CLOSED,
+    CONN_STATE_ERROR
+} ConnectionState;
+
+typedef struct ConnectionType {
+    void (*ae_handler)(struct _eventloop *el, int fd, void *clientData, int mask);
+    int (*connect)(struct connection *conn, const char *addr, int port, const char *source_addr, ConnectionCallbackFunc connect_handler);
+    int (*write)(struct connection *conn, const void *data, size_t data_len);
+    int (*read)(struct connection *conn, void *buf, size_t buf_len);
+    void (*close)(struct connection *conn);
+    int (*accept)(struct connection *conn, ConnectionCallbackFunc accept_handler);
+    int (*set_write_handler)(struct connection *conn, ConnectionCallbackFunc handler, int barrier);
+    int (*set_read_handler)(struct connection *conn, ConnectionCallbackFunc handler);
+    const char *(*get_last_error)(struct connection *conn);
+    int (*blocking_connect)(struct connection *conn, const char *addr, int port, long long timeout);
+    ssize_t (*sync_write)(struct connection *conn, char *ptr, ssize_t size, long long timeout);
+    ssize_t (*sync_read)(struct connection *conn, char *ptr, ssize_t size, long long timeout);
+    ssize_t (*sync_readline)(struct connection *conn, char *ptr, ssize_t size, long long timeout);
+    int (*get_type)(struct connection *conn);
+} ConnectionType;
+
+struct connection
+{
+    ConnectionType *type;
+    ConnectionState state;
+    short int flags;
+    short int refs;
+    int last_errno;
+    void *private_data;
+    ConnectionCallbackFunc conn_handler;
+    ConnectionCallbackFunc write_handler;
+    ConnectionCallbackFunc read_handler;
+    int fd;
+};
 
 // set up new NON BLOCKING listening socket using port and return socketfd
 int init_conn(int port);
